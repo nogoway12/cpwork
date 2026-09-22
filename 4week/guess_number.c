14e45fde15d6a7cf9c1e1c507d9e47a2e69028f2
nogoway12 <nogoway12@gmail.com>
2026-09-22 10:45:10 +0900

    Remove comments as requested

--- 4week/guess_number.c
+++ 4week/guess_number.c
@@ -7,12 +7,10 @@ int main()
 {
     int num;
     int answer;
-    int count = 0; // 시도 횟수를 저장할 변수
+    int count = 0;
 
-    // Windows 콘솔에서 한글 출력을 위한 설정
     system("chcp 65001 > nul");
 
-    // 난수 초기화 및 정답 생성 (예: 1부터 100 사이의 숫자)
     srand(time(NULL));
     answer = rand() % 100 + 1; 
 
@@ -21,7 +19,7 @@ int main()
         printf("숫자 입력: ");
         scanf("%d", &num);
         
-        count++; // 숫자를 입력할 때마다 카운트 1 증가
+        count++;
 
         if(num > answer){
             printf("더 작은 수입니다.\n");
@@ -31,7 +29,6 @@ int main()
         }
         else{
             printf("정답입니다. \n");
-            // 정답을 맞췄을 때 총 시도 횟수 출력
             printf("총 %d번 만에 맞췄습니다!\n", count);
             break;
         }
